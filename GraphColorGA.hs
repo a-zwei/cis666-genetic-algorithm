d9b74e8cf79e2d1c660bf9a7f22f1163870c55aa
Fred Maurer <fmaurer+github@gmail.com>
2010-11-24 10:10:34 -0500

    almost done

--- GraphColorGA.hs
+++ GraphColorGA.hs
@@ -1,8 +1,10 @@
 module GraphColorGA (Color, {-colorGraph,-} nodeColor) where
 
-import Data.Array.Unboxed
-import Data.List (nub)
-import System.Random
+import Control.Monad (replicateM)
+import Data.Function (on)
+import Data.Ix (inRange)
+import Data.List (findIndex, nub, sortBy)
+import System.Random (randomIO, randomRIO)
 
 import UGraph
 import Util
@@ -13,46 +15,96 @@ type Solution = [Color]
 
 type Population = [Solution]
 
-a = 100
-b = 100
+type Pair = (Solution, Solution)
+
+a = 500
+b = 5
 
 fitness :: UGraph -> Solution -> Float
-fitness g s = a / numColors s - b * adjacentSame g s
+fitness g s = max 0 $ a / numColors s - b * adjacentSame g s
 
 adjacentSame :: UGraph -> Solution -> Float
 adjacentSame g s = sum [1 | n <- nodes g,
   nodeColor s n `elem` neighborColors g s n]
 
+nodeColor :: Solution -> Node -> Color
+nodeColor s n = s !! (n - 1) -- assuming nodes are [1..]
+
 numColors :: Solution -> Float
 numColors = fromIntegral . length . nub
 
-cross :: Int -> Solution -> Solution -> (Solution, Solution)
-cross n s1 s2 = (take n s1 ++ drop n s2, take n s2 ++ drop n s1)
+cross :: Int -> Pair -> Pair
+cross n (s1, s2) = (take n s1 ++ drop n s2, take n s2 ++ drop n s1)
 
-mutate :: Int -> Int -> Solution -> Solution
-mutate ix n s = update ix (color `mod` maximum s) s
-  where color = if even n then s !! ix + 1 else s !! ix - 1
+crossIO :: Pair -> IO Pair
+crossIO sp = do
+  n <- randomRIO (1, (length . fst) sp - 1)
+  return $ cross n sp
+
+mutate :: Int -> Solution -> Solution
+mutate ix s = update ix (color `mod` maximum s) s
+  where color = s !! ix - 1
+
+mutateIO :: Solution -> IO Solution
+mutateIO s = do
+  ix <- randomRIO (0, length s - 1)
+  return $ mutate ix s
 
 probs :: UGraph -> Population -> [Float]
 probs g p = [fitness g s / totalFitness | s <- p]
   where totalFitness = sum $ map (fitness g) p
 
---update :: UGraph -> Population -> Population
---update g s = State $ array (bounds $ us s) (map updateU $ assocs (us s))
---  where updateU ((i, j), u) = ((i, j), u + dt * dudt g s (i, j))
+probRanges :: UGraph -> Population -> [(Int, Int)]
+probRanges g p = zip ms (map (subtract 1) $ tail ms)
+  where ms = scanl f 0 $ probs g p
+        f t p = floor (10000 * p) + t
 
-randomSolution :: Int -> Int -> Int -> Solution
-randomSolution n max seed = take n $ randomRs (1, max) (mkStdGen seed)
+select :: UGraph -> Population -> IO Solution
+select g p = do
+  n <- randomRIO (0, 9999)
+  let ix = fromJust $ findIndex (flip inRange n) (probRanges g p)
+  return $ p !! ix
 
-nodeColor :: Solution -> Node -> Color
-nodeColor s n = s !! n - 1 -- assuming nodes are [1..]
+matingPool :: UGraph -> Population -> IO Population
+matingPool g p = replicateM (length p) $ select g p
+
+pairOff :: Population -> [Pair]
+pairOff [] = []
+pairOff (s1:s2:ss) = (s1, s2) : pairOff ss
+
+nextGeneration :: UGraph -> Population -> IO Population
+nextGeneration g p = do
+  mp <- matingPool g p
+  ngps <- mapM crossIO $ pairOff mp
+  return $ flatten ngps
+
+randomSolution :: Int -> Int -> IO Solution
+randomSolution n max = replicateM n $ randomRIO (1, max)
+
+randomPopulation :: Int -> Int -> Int -> IO Population
+randomPopulation n sl max = replicateM n $ randomSolution sl max
 
 neighborColors :: UGraph -> Solution -> Node -> [Color]
 neighborColors g s node = [nodeColor s n | n <- neighbors g node]
 
---colorGraph :: UGraph -> Int -> Solution
---colorGraph g seed = states !! localMin energies
---  where initialState = randomState (noNodes g) (noNodes g) seed
---        states = filter valid $ iterate (update g) initialState
---        energies = map (energy g) states
---        valid s = allColored s && neighborsDiffer g s
+best :: UGraph -> Population -> Solution
+best g p = last $ sortBy (compare `on` (fitness g)) p
+
+repeatGA :: UGraph -> Int -> Int -> Population -> IO Population
+repeatGA g times regressChances p = do
+  newPopulation <- iterateM times (nextGeneration g) p
+  let oldBest = best g p
+  let newBest = best g newPopulation
+  putStr $ "Best after " ++ show times ++ " iterations: " ++ show newBest
+  putStrLn $ " (fitness " ++ show (fitness g newBest) ++ ")"
+  if fitness g newBest > fitness g oldBest
+    then repeatGA g times regressChances newPopulation
+    else if regressChances > 0
+            then repeatGA g times (regressChances - 1) newPopulation
+            else return p
+
+colorGraph :: UGraph -> Int -> Int -> Int -> IO Solution
+colorGraph g popSize iters regressChances = do
+  initialPopulation <- randomPopulation popSize (noNodes g) (noNodes g)
+  newPopulation <- repeatGA g iters regressChances initialPopulation
+  return $ best g newPopulation
--- Util.hs
+++ Util.hs
@@ -1,5 +1,7 @@
 module Util where
 
+import Control.Monad (foldM)
+
 boolToInt True = 1
 boolToInt False = 0
 
@@ -8,3 +10,9 @@ fromJust (Just a) = a
 update ix v vs = take ix vs ++ v : drop (ix + 1) vs
 
 clamp (l, u) v = min u $ max l v
+
+flatten [] = []
+flatten ((a, b):xs) = a:b:flatten xs
+
+iterateM :: Monad m => Int -> (a -> m a) -> a -> m a
+iterateM n mf init = foldM (\p m -> m p) init (replicate n mf)
